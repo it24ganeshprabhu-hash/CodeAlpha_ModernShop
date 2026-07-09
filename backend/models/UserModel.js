db79a0af1527eeb1340f6bc960b9c8176c29e6fd
it24ganeshprabhu-hash <it24.ganesh.prabhu@kccemsr.edu.in>
2026-07-09 13:00:31 +0530

    JWT Backend login

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,5 +1,7 @@
 let {MongoClient , ObjectId, ClientEncryption} = require("mongodb");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "your_super_secret_key";
 let url = process.env.MONGO_URI;
 
 let Client = new MongoClient(url);
@@ -44,7 +46,8 @@ let LoginUser = async (req, res) => {
         const isMatch = await bcrypt.compare(req.body.password, user.password);
 
         if (isMatch) {
-            res.status(200).json({ message: "User Logged In Successfully"});
+            const token = jwt.sign({ username: user.username }, JWT_SECRET, { expiresIn: '1h' });
+            res.status(200).json({ message: "Login successful", token: token });
         } else {
             res.status(401).json({ message: "Invalid Username or Password" });
         }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -5,7 +5,7 @@ const CartController = require("../controllers/CartController");
 let router = express.Router();
 
 router.post("/register",UserController.RegisterUser);
-router.get("/login",UserController.LoginUser);
+router.post("/login",UserController.LoginUser);
 router.post("/cart/add",CartController.AddToCart);
 router.get("/cart/items", CartController.GetCartItems);
 router.delete("/cart/remove",CartController.RemoveFromCart);
